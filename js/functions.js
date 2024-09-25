88e179e7d8459d2f8a3d55b0cdd2312d0ff4bd16
Zeshma <jere.makivierikko@gmail.com>
2024-09-25 21:21:52 +0300

    Muokattu tyylejä ja vihdoin saatu jokeri rivien väliin tyhjää! Se olikin tableen border-spacing eikä td osioon.

--- js/functions.js
+++ js/functions.js
@@ -1,22 +1,22 @@
-const newRowButton = document.getElementById('newRowButton');
-const jokerTable = document.getElementById('jokerTable');
-const rowCountElement = document.getElementById('rowCount');
+const newRowButton = document.getElementById('newRowButton')
+const jokerTable = document.getElementById('jokerTable')
+const rowCountElement = document.getElementById('rowCount')
 
-let rowCount = 0;
+let rowCount = 0
 
 newRowButton.addEventListener('click', function() {
-    addJokerRow();
-});
+    addJokerRow()
+})
 
 function addJokerRow() {
-    const row = jokerTable.insertRow();
+    const row = jokerTable.insertRow()
 
     for (let i = 0; i < 7; i++) {
-        const cell = row.insertCell();
-        cell.textContent = Math.floor(Math.random() * 10);
-        cell.classList.add('jokerCell');
+        const cell = row.insertCell()
+        cell.textContent = Math.floor(Math.random() * 10)
+        cell.classList.add('jokerCell')
     }
 
-    rowCount++;
-    rowCountElement.textContent = rowCount;
+    rowCount++
+    rowCountElement.textContent = rowCount
 }
